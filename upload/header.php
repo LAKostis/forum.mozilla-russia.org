c9604c9355b5a6f6fda8c3aa576be53d27b554bd
cvs_admin <cvs_admin>
2006-03-18 19:17:04 +0000

    - semi-update to 1.2.11 (see http://dev.punbb.org/changeset/335).

--- upload/header.php
+++ upload/header.php
@@ -165,7 +165,7 @@ if (isset($focus_element))
 
 
 // START SUBST - <pun_page>
-$tpl_main = str_replace('<pun_page>', basename($_SERVER['PHP_SELF'], '.php'), $tpl_main);
+$tpl_main = str_replace('<pun_page>', htmlspecialchars(basename($_SERVER['PHP_SELF'], '.php')), $tpl_main);
 // END SUBST - <pun_title>
 
 

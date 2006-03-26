57a2797313332e174c102ddde3f41b31a24c5f9e
Konstantin A. Lepikhov <lakostis @ mozilla-russia>
2006-03-26 21:42:48 +0000

    - fix #18 (add easy_bbcode support to PM system).

--- upload/message_send.php
+++ upload/message_send.php
@@ -252,6 +252,7 @@ else
 				<label class="conl"><strong><?php echo $lang_pms['Send to'] ?></strong><br /><?php echo '<input type="text" name="req_username" size="25" maxlength="25" value="'.pun_htmlspecialchars($username).'" tabindex="'.($cur_index++).'" />'; ?><br /></label>
 				<div class="clearer"></div>
 				<label><strong><?php echo $lang_common['Subject'] ?></strong><br /><input class="longinput" type='text' name='req_subject' value='<?php echo $subject ?>' size="80" maxlength="70" tabindex='<?php echo $cur_index++ ?>' /><br /></label>
+				<?php require PUN_ROOT.'mod_easy_bbcode.php'; ?>
 				<label><strong><?php echo $lang_common['Message'] ?></strong><br />
 				<textarea name="req_message" rows="20" cols="95" tabindex="<?php echo $cur_index++ ?>"><?php echo $quote ?></textarea><br /></label>
 				<ul class="bblinks">

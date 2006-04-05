2bb27587b0d1225dc59745ea369a1139211a8a21
Konstantin A. Lepikhov <lakostis @ mozilla-russia>
2006-04-05 21:24:57 +0000

    - don't display PM link to myself ;)

--- upload/include/pms/viewtopic_PM-link.php
+++ upload/include/pms/viewtopic_PM-link.php
@@ -1,7 +1,7 @@
 <?
 	require PUN_ROOT.'lang/'.$pun_user['language'].'/pms.php';
 
-	if($pun_config['o_pms_enabled'] && !$pun_user['is_guest'])
+	if($pun_config['o_pms_enabled'] && !$pun_user['is_guest'] && ($cur_post['poster_id'] != $pun_user['id']))
 	{
 		$pid = isset($cur_post['poster_id']) ? $cur_post['poster_id'] : $cur_post['id'];
 		$user_contacts[] = '<a href="message_send.php?id='.$pid.'&amp;tid='.$id.'">'.$lang_pms['PM'].'</a>';

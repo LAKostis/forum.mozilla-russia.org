8da5326e3919c7da576389d381f8cdb0fa8dfd9b
Alexander L. Slovesnik <unghost @ mozilla-russia>
2007-10-16 17:58:43 +0000

    Add Omniweb icon

--- upload/include/functions.php
+++ upload/include/functions.php
@@ -1406,6 +1406,7 @@ function pun_get_browser($uagent){
                      'links',
                      'lolifox',
                      'lynx',
+                     'omniweb',
                      'shiira',
                      'songbird',
                      'swift');

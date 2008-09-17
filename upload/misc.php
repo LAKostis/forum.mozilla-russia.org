ddb90a3b528cb13854700d70477fa5ce4e7eeff3
Konstantin A. Lepikhov <lakostis@mozilla-russia.org>
2008-09-18 02:10:37 +0400

    Fix typo in mark forum as read code.

--- upload/misc.php
+++ upload/misc.php
@@ -30,7 +30,6 @@ if (isset($_GET['action']))
 define('PUN_ROOT', './');
 require PUN_ROOT.'include/common.php';
 
-
 // Load the misc.php language file
 require PUN_ROOT.'lang/'.$pun_user['language'].'/misc.php';
 
@@ -82,7 +81,7 @@ else if ($action == 'markforumread')
 		message($lang_common['Bad request']);
 
        // Make sure the user can view the topic
-       $result = $db->query('SELECT 1 FROM '.$db->prefix.'topics AS t LEFT JOIN '.$db->prefix.'forum_perms AS fp ON (fp.forum_id=t.forum_id AND fp.group_id=1) WHERE (fp.read_forum IS NULL OR fp.read_forum=1) AND t.id='.$topic_id.' AND t.moved_to IS NULL') or error('Unable to fetch topic info', __FILE__, __LINE__, $db->error());
+       $result = $db->query('SELECT 1 FROM '.$db->prefix.'topics AS t LEFT JOIN '.$db->prefix.'forum_perms AS fp ON (fp.forum_id=t.forum_id AND fp.group_id=1) WHERE (fp.read_forum IS NULL OR fp.read_forum=1) AND t.forum_id='.$id.' AND t.moved_to IS NULL') or error('Unable to fetch topic info', __FILE__, __LINE__, $db->error());
        if (!$db->num_rows($result))
                message($lang_common['Bad request']);
 

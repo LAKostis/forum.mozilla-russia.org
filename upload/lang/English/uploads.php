13d9c1348f43889e581a22a5da68a92da94801c7
Nikolay S. Frantsev <shutnik@mozilla-russia.org>
2009-04-25 00:08:43 +0600

    fix english uploader locale

--- upload/lang/English/uploads.php
+++ upload/lang/English/uploads.php
@@ -3,11 +3,15 @@
 // Language definitions used in uploads.php
 $lang_uploads = array(
 
-'Uploader'			=>	'Uploader',
-'File'			=>	'File',
-'Size'			=>	'Size',
-'Delete'			=>	'Delete',	// The submit button
-'Posted by'			=>	'Posted by',
+'Uploader'				=>	'Uploader',
+'Upload'				=>	'Upload',
+'New file upload'		=>	'Upload new file',
+'File'					=>	'File',
+'File list'				=>	'Files list',
+'File list info'		=>	'Files, uploaded by users, may be harmful and/or contain errors. If you want to download a file, just click on the link. If you found a file with dangerous contents, please notify forum\'s administration.',
+'Size'					=>	'Size',
+'Delete'				=>	'Delete',	// The submit button
+'Posted by'				=>	'Posted by',
 'Topic del redirect'	=>	'Topic deleted. Redirecting &hellip;'
 
 );

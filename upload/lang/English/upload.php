658d29fe596020948165648f60fe1a1511cc902d
Alexander L. Slovesnik <unghost@mozilla-russia.org>
2010-11-03 00:25:13 +0300

    Remove extra commas

--- upload/lang/English/upload.php
+++ upload/lang/English/upload.php
@@ -8,6 +8,6 @@ $lang_upload = array(
 'Rule one'		=>	'You can use the upload to upload image and text with the following extention. (.txt, .gif, .jpg, .jpeg, .png, .xpi)',
 'Rule two'					=>	'The file must be under',
 'Rule three'				=>	'The file must contain only latin characters like alphanumeric and &quot;+&quot; &quot;-&quot; &quot;_&quot; &quot;.&quot;',
-'Rule four'		=>	'Please use a sensible file name or i will remove the file, if anyone abuses this uploader, it will be removed.',
+'Rule four'		=>	'Please use a sensible file name or i will remove the file, if anyone abuses this uploader, it will be removed.'
 
 );

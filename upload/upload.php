bcaf3472bee81374657001be9c6010ed0ae38f30
Alexander L. Slovesnik <unghost@mozilla-russia.org>
2010-11-03 00:25:07 +0300

    Translate upload rules

--- upload/upload.php
+++ upload/upload.php
@@ -3,6 +3,7 @@
 define('PUN_ROOT', './');
 require PUN_ROOT.'include/common.php';
 require PUN_ROOT.'lang/'.$pun_user['language'].'/'.'uploads.php';
+require PUN_ROOT.'lang/'.$pun_user['language'].'/'.'upload.php';
 
 if ($pun_user['g_read_board'] == '0')
 	message($lang_common['No view']);
@@ -19,10 +20,10 @@ require PUN_ROOT.'header.php';
 if (!$upl_conf['p_view'] || $pun_user['is_guest']) {
 	?>
 	<div id="announce" class="block">
-		<h2><span><b>Not allowed</b></span></h2>
+		<h2><span><b><?php echo $lang_upload['Not allowed'] ?></b></span></h2>
 		<div class="box">
 			<div class="inbox">
-				<div><?php      echo '<b>You do not have permissons to access upload module. Please, contact Administration.</b>'; ?>
+				<div><?php      echo '<b>You do not have permissions to access upload module. Please, contact Administration.</b>'; ?>
 				</div>
 			</div>
 		</div>
@@ -35,11 +36,11 @@ if (!$upl_conf['p_view'] || $pun_user['is_guest']) {
 	<legend><?php echo $lang_uploads['New file upload'] ?></legend>
 			<div class="infldset">
 				<form method="POST" action="uploads.php" enctype="multipart/form-data">
-				<p><b>Uploading Rules:</b><br />
-					- You can use the upload to upload image and text with the following extention. (.txt, .gif, .jpg, .jpeg, .png, .xpi)<br>
-					- The file must be under <?php echo round($upl_conf['u_fsize'] / 1024).'KB'; ?> <br>
-					- The file must contain only latin characters like alphanumeric and &quot;+&quot; &quot;-&quot; &quot;_&quot; &quot;.&quot; <br>
-					- Please use a sensible file name or i will remove the file, if anyone abuses this uploader, it will be removed.<br><br>
+				<p><b><?php echo $lang_upload['Not allowed'] ?>:</b><br />
+					- <?php echo $lang_upload['Rule one'] ?><br>
+					- <?php echo $lang_upload['Rule two'] ?> <?php echo round($upl_conf['u_fsize'] / 1024).'KB'; ?> <br>
+					- <?php echo $lang_upload['Rule three'] ?>  <br>
+					- <?php echo $lang_upload['Rule four'] ?><br><br>
 					<input type="file" name="file" size="30"><br><br>
 					<input type="hidden" name="user_id" value="<?php echo $pun_user['id']; ?>">
 					<input type="hidden" name="user_name" value="<?php echo $pun_user['username']; ?>">

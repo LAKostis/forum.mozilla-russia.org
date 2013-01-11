3f699821fcaa988e14e3d944c4c80effcf55ba11
Alexander L. Slovesnik <unghost@mozilla-russia.org>
2013-01-11 20:46:23 +0400

    Switch Firefox 17 to old icon
    Signed-off-by: Konstantin A. Lepikhov <lakostis@mozilla-russia.org>

--- upload/include/functions.php
+++ upload/include/functions.php
@@ -1583,7 +1583,7 @@ function pun_get_browser($uagent) {
 		}
 		if($ua_browser == "ie")
 			$ua_browser_alt = "IE";
-		if ($ua_browser == "firefox" && $ua_version >= "17.0") {
+		if ($ua_browser == "firefox" && $ua_version >= "18.0") {
 			$ua_browser = "firefox35";
 			$ua_browser_alt = "FIREFOX";
 		}
